4e1cd28ccb1f5e5e58dd87ca2b3b6d8f7b9d13f2
dmambapoor <dmambapoor@gmail.com>
2023-03-21 14:09:15 -0500

    Experimenting with data

--- src/liver_cirrhosis_prediction/lc_data_prep.py
+++ src/liver_cirrhosis_prediction/lc_data_prep.py
@@ -3,3 +3,6 @@ import pandas as pd     # type: ignore
 data = pd.read_csv('src/liver_cirrhosis_prediction/cirrhosis.csv')
 
 print(data.head())
+
+for col in data.columns:
+    print(data[col].isnull().value_counts())
